f8b4fb8eaf22dbfedfef1d4601bfd5228766453b
sourav louha <souravlouha00@gail.com>
2025-01-17 19:49:15 +0530

    up

--- Basic_py_forML/first.py
+++ Basic_py_forML/first.py
@@ -1,3 +1,18 @@
 print("hello world") # .py is the extention for pythone language , other example is like , video /movie - .mp4 another one is game file or text file - .exe , etc 
 
 ''' MODULE - In Python, a module is a file containing Python code, such as functions, classes, and variables. It's a way to organize and reuse code, making your projects more manageable and maintainable.'''
+
+
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!''')
+ # jokhon amra akta print function a ,multiple lines likhbo tar jonne , amader triple single inverted commas (''' ''') use korte hobe , nahole amader protita liner jonne alada alada kore print function likhte hoto  
