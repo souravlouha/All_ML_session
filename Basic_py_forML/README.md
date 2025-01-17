f2b2c65a0f332aabc5788b46c9743e49306a44bc
sourav louha <souravlouha00@gail.com>
2025-01-17 18:55:33 +0530

    update

---
+++ Basic_py_forML/README.md
@@ -0,0 +1,2 @@
+https://media.licdn.com/dms/image/v2/D5612AQGZWhXwKoTkFg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1675748802083?e=1742428800&v=beta&t=d91_T6iVIaasZblYOPxigG_0bMXueZ60n-VGMV_4rfk
+
---
+++ Basic_py_forML/first.py
@@ -0,0 +1,3 @@
+print("hello world") # .py is the extention for pythone language , other example is like , video /movie - .mp4 another one is game file or text file - .exe , etc 
+
+''' MODULE - In Python, a module is a file containing Python code, such as functions, classes, and variables. It's a way to organize and reuse code, making your projects more manageable and maintainable.'''
